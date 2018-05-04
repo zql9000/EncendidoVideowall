09b1ff6bbfc55875d50c56dbf2450f7ad3cf4fbb
zql9000 <zql9000@users.noreply.github.com>
2018-05-04 18:43:37 -0300

    Primer version

---
+++ EncendidoVideowall/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace EncendidoVideowall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPuerto = new System.Windows.Forms.ComboBox();
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Puerto:";
+            // 
+            // cmbPuerto
+            // 
+            this.cmbPuerto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPuerto.FormattingEnabled = true;
+            this.cmbPuerto.Location = new System.Drawing.Point(67, 12);
+            this.cmbPuerto.Name = "cmbPuerto";
+            this.cmbPuerto.Size = new System.Drawing.Size(121, 21);
+            this.cmbPuerto.Sorted = true;
+            this.cmbPuerto.TabIndex = 1;
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.Location = new System.Drawing.Point(16, 83);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(116, 105);
+            this.btnEncender.TabIndex = 2;
+            this.btnEncender.Text = "Encender";
+            this.btnEncender.UseVisualStyleBackColor = true;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Location = new System.Drawing.Point(156, 83);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(116, 105);
+            this.btnApagar.TabIndex = 3;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 212);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnEncender);
+            this.Controls.Add(this.cmbPuerto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Encedido de Videowall";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPuerto;
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagar;
+    }
+}
+
---
+++ EncendidoVideowall/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace EncendidoVideowall
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ComPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CrearPuerto()
+        {
+            ComPort = new SerialPort();
+
+            ComPort.BaudRate = 9600;
+            ComPort.Parity = Parity.None;
+            ComPort.DataBits = 8;
+            ComPort.StopBits = StopBits.One;
+            ComPort.Handshake = Handshake.None;
+            ComPort.ReadTimeout = 500;
+            ComPort.WriteTimeout = 500;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CrearPuerto();
+
+            foreach (var item in SerialPort.GetPortNames())
+            {
+                cmbPuerto.Items.Add(item);
+            }
+
+            if (cmbPuerto.Items.Count > 0)
+                cmbPuerto.SelectedItem = cmbPuerto.Items[0];
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            if (cmbPuerto.SelectedItem == null)
+                MessageBox.Show("Debe seleccionar el puerto donde se encuantra conectado el Videowall","Puerto");
+            else
+                EnviarComando(cmbPuerto.Text, "K:000PON.K:016PON.K:001PON.K:017PON.");
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            if (cmbPuerto.SelectedItem == null)
+                MessageBox.Show("Debe seleccionar el puerto donde se encuantra conectado el Videowall", "Puerto");
+            else
+                EnviarComando(cmbPuerto.Text, "K:000POF.K:016POF.K:001POF.K:017POF.");
+        }
+
+        private void EnviarComando(string Puerto, string Comando)
+        {
+            try
+            {
+                ComPort.PortName = Puerto;
+                ComPort.Open();
+                ComPort.Write(Comando);
+                ComPort.Close();
+            }
+            catch (Exception)
+            {
+                CrearPuerto();
+            }
+        }
+    }
+}
